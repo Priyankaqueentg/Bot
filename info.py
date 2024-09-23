3235553dc56eec2d98fcd375e6b39376fa4de621
Priyankaqueentg <officialwildcash4@gmail.com>
2024-09-23 22:51:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,6 @@ def is_enabled(value, default):
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '26395468'))
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
 API_HASH = environ.get('API_HASH', '680786593eb3e6b3195153f1ae9dd17e')
 BOT_TOKEN = environ.get('BOT_TOKEN', "6232508540:AAH_Q5iJRpUt1vYGTIhVZUihgaxlMz63dNs")
 
